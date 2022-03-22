bae734a0aae8486b6ce5e023e7d3f5d1b3e5fc97
chick_0 <chick_0@ch1ck.xyz>
2022-03-22 22:43:05 +0900

    api!

---
+++ app/utils.py
@@ -0,0 +1,57 @@
+from functools import wraps
+
+from flask import request
+from flask import session
+from flask import jsonify
+
+from app.models import Vote
+from app.const import VOTE_ADMIN
+
+
+def resp(message: str = "", data: dict or list = None, code: int = 200):
+    return jsonify({
+        "message": message,
+        "data": data,
+    }), code
+
+
+def vote_filter(vote: Vote):
+    if not session.get(vote.id, "") == VOTE_ADMIN:
+        return resp(
+            message="권한이 없습니다.",
+            code=403
+        )
+
+    if vote.started:
+        return resp(
+            message="이미 시작된 투표입니다.",
+            code=400
+        )
+
+    return None
+
+
+def fetch_vote(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        vote_id = request.args.get("vote_id")
+        if vote_id is None:
+            return resp(
+                message="투표 아이디를 전달받지 못했습니다.",
+                code=400
+            )
+
+        v: Vote = Vote.query.filter_by(
+            id=vote_id
+        ).first()
+
+        if v is None:
+            return resp(
+                message="등록된 투표가 아닙니다.",
+                code=404
+            )
+
+        kwargs.update({"vote": v})
+        return f(*args, **kwargs)
+
+    return decorator
--- app/views/api.py
+++ app/views/api.py
@@ -1,35 +1,145 @@
 from flask import Blueprint
 from flask import session
 from flask import request
-from flask import jsonify
+
+from app import db
+from app.models import Vote
+from app.models import Option
+from app.utils import resp
+from app.utils import fetch_vote
+from app.utils import vote_filter
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
 
-def resp(message: str = "", data: dict = None, code: int = 200):
-    return jsonify({
-        "message": message,
-        "data": data,
-    }), code
-
-
-@bp.post("/opt")
-def new_option():
-    vote_id = request.args.get("vote_id", None)
-    if vote_id is None:
+@bp.get("/opts")
+@fetch_vote
+def get_options(vote: Vote):
+    if session.get(vote.id) is None:
         return resp(
-            message="vote_id is missing",
-            code=400
+            message="권한이 없습니다.",
+            code=403
         )
 
     return resp(
+        data=[
+            {
+                "id": x.id,
+                "name": x.name,
+            } for x in Option.query.filter_by(
+                vote_id=vote.id
+            ).all()
+        ]
+    )
+
+
+@bp.post("/opt")
+@fetch_vote
+def new_option(vote: Vote):
+    vf = vote_filter(vote=vote)
+    if vf is not None:
+        return vf
+
+    name = request.json.get("name", "").strip()[:30]
+    if len(name) == 0:
+        return resp(
+            message="이름없는 선택지는 등록할 수 없습니다.",
+            code=400
+        )
+
+    o = Option()
+    o.vote_id = vote.id
+    o.name = name
+
+    db.session.add(o)
+    db.session.commit()
+
+    return resp(
+        message="새로운 선택지를 등록했습니다.",
         data={
-            "option_id": "q"
+            "option_id": o.id,
         },
         code=201,
     )
 
 
-@bp.delete("/opt/<string:option_id>")
-def delete_option(option_id: str):
-    return "q"
+@bp.post("/opt/<int:option_id>")
+@fetch_vote
+def edit_option(option_id: int, vote: Vote):
+    vf = vote_filter(vote=vote)
+    if vf is not None:
+        return vf
+
+    name = request.json.get("name", "").strip()[:30]
+    if len(name) == 0:
+        Option.query.filter_by(
+            id=option_id,
+            vote_id=vote.id
+        ).delete()
+        db.session.commit()
+
+        return resp(
+            message="해당 선택지를 삭제했습니다.",
+            code=400
+        )
+
+    o = Option.query.filter_by(
+        id=option_id,
+        vote_id=vote.id
+    ).first()
+
+    o.name = name
+    db.session.commit()
+
+    return resp(
+        message="해당 선택지를 수정했습니다.",
+        code=200
+    )
+
+
+@bp.delete("/opt/<int:option_id>")
+@fetch_vote
+def delete_option(option_id: int, vote: Vote):
+    vf = vote_filter(vote=vote)
+    if vf is not None:
+        return vf
+
+    Option.query.filter_by(
+        id=option_id,
+        vote_id=vote.id
+    ).delete()
+    db.session.commit()
+
+    return resp(
+        message="해당 선택지를 삭제했습니다.",
+        code=200
+    )
+
+
+@bp.delete("/opt/<int:option_id>")
+@fetch_vote
+def fetch_option(option_id: int, vote: Vote):
+    if session.get(vote.id) is None:
+        return resp(
+            message="권한이 없습니다.",
+            code=403
+        )
+
+    o = Option.query.filter_by(
+        id=option_id,
+        vote_id=vote.id
+    ).first()
+
+    if o is None:
+        return resp(
+            message="등록된 선택지가 아닙니다.",
+            code=400
+        )
+
+    return resp(
+        data={
+            "id": o.id,
+            "name": o.name,
+        },
+        code=200
+    )
