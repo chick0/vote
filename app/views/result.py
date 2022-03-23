ef079ef65ce3da3e1e7fb95e272e587d4dcb8f01
chick_0 <chick_0@ch1ck.xyz>
2022-03-23 16:26:59 +0900

    do not remove session id

--- app/views/result.py
+++ app/views/result.py
@@ -1,6 +1,4 @@
 from flask import Blueprint
-from flask import session
-from flask import abort
 from flask import render_template
 
 bp = Blueprint("result", __name__, url_prefix="/result")
@@ -8,11 +6,6 @@ bp = Blueprint("result", __name__, url_prefix="/result")
 
 @bp.get("/vote/<int:vote_id>/end")
 def end(vote_id: int):
-    try:
-        del session[str(vote_id)]
-    except KeyError:
-        return abort(404)
-
     return render_template(
         "result/end.html",
     )
