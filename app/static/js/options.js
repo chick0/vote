f6a7253fb589da4e7d0da38a1e0a6c0c23b87b6f
chick_0 <chick_0@ch1ck.xyz>
2022-04-03 12:26:01 +0900

    add option delete handler

--- app/static/js/options.js
+++ app/static/js/options.js
@@ -1,11 +1,58 @@
 function render_display(){
+    function err(message) {
+        Swal.fire({
+            icon: "error",
+            text: message,
+            showCancelButton: false,
+            showConfirmButton: true,
+            confirmButtonText: "닫기",
+            timer: 2022,
+            timerProgressBar: true
+        });
+    }
+
     const display = document.getElementById("display");
     display.innerHTML = "";
+
+    const handler = (event) => {
+        const target = event.target;
+        Swal.fire({
+            icon: "question",
+            text: "해당 선택지를 삭제하시겠습니까?",
+            showCancelButton: true,
+            showConfirmButton: true,
+            confirmButtonText: "네",
+            cancelButtonText: "아니요",
+        }).then((result) => {
+            if(result.isConfirmed){
+                axios({
+                    method: "DELETE",
+                    url: "/api/opt",
+                    params: {
+                        vote_id: vote_id,
+                        option_id: target.dataset.id,
+                    },
+                }).then(() => {
+                    delete opts[target.dataset.id]
+                    render_display();
+                }).catch((error) => {
+                    const resp = error.response;
+                    if(resp == undefined){
+                        err("알 수 없는 오류가 발생했습니다.");
+                    } else {
+                        err(resp.data.message);
+                    }
+                });
+            }
+        });    
+    };
 
     Object.keys(opts).forEach((id) => {
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(opts[id]));
-        li.setAttribute("data-opt-id", id);
+        li.setAttribute("data-id", id);
+        li.setAttribute("style", "padding-bottom:20px;");
+        li.addEventListener("click", handler);
 
         display.appendChild(li);
     });
