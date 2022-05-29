727b252ff09b56296833cfd016fb22951d5c4fdb
chick_0 <chick_0@ch1ck.xyz>
2022-05-30 02:23:24 +0900

    upgrade worker

--- worker.py
+++ worker.py
@@ -2,103 +2,105 @@
 from os import environ
 from time import time
 from time import sleep
+from sched import scheduler
+from functools import wraps
 from datetime import datetime
 from datetime import timedelta
 from logging import INFO
 from logging import getLogger
-from logging import Formatter
 from logging import StreamHandler
+from logging import Formatter
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
 from app.models import Vote
 from app.models import Session
 from app.models import Option
 
-engine = None
+logger = getLogger()
+s = scheduler(time, sleep)
+schedule = {}
 
 
-def init_engine():
-    load_dotenv()
-    globals().update({
-        "engine": create_engine(environ.get("SQLALCHEMY_DATABASE_URI"))
-    })
+def register_schedule(delay: int):
+    def wrapper(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            s.enter(**schedule[f.__name__])
+            return f(*args, **kwargs)
 
+        schedule.update({
+            f.__name__: {
+                "delay": delay,
+                "priority": 1,
+                "action": decorator
+            }
+        })
 
-def init_logger():
-    logger = getLogger()
-    logger.setLevel(INFO)
-
-    fmt = Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
-    sh = StreamHandler()
-    sh.setFormatter(fmt)
-
-    logger.addHandler(sh)
-
-
-def e(message: str) -> None:
-    logger = getLogger()
-    logger.info(message)
+        decorator()
+        return decorator
+    return wrapper
 
 
 def main():
-    def delete_all(vote_id):
-        session.query(Vote).filter_by(
-            id=vote_id
-        ).delete()
-        session.query(Session).filter_by(
-            vote_id=vote_id
-        ).delete()
-        session.query(Option).filter_by(
-            vote_id=vote_id
-        ).delete()
+    @register_schedule(timedelta(minutes=10).seconds)
+    def clear_vote():
+        def delete_all(vote_id):
+            session.query(Vote).filter_by(
+                id=vote_id
+            ).delete()
+            session.query(Session).filter_by(
+                vote_id=vote_id
+            ).delete()
+            session.query(Option).filter_by(
+                vote_id=vote_id
+            ).delete()
 
-        session.commit()
+            session.commit()
 
-    a = time()
-    session = sessionmaker(bind=engine)()
+        session = sessionmaker(bind=engine)()
 
-    now = datetime.now()
+        now = datetime.now()
 
-    total_votes = session.query(Vote).count()
-    deleted_votes = 0
-    page = 5
-    total_pages = getattr(__import__("math"), "ceil")(total_votes / page)
+        total_votes = session.query(Vote).count()
+        deleted_votes = 0
+        per_page = 5
+        total_pages = getattr(__import__("math"), "ceil")(total_votes / per_page)
 
-    e(f"total_votes   = {total_votes}")
+        logger.info(f"total votes : {total_pages}")
+        logger.info(f"total pages : {total_pages}")
 
-    last_id = 0
-    for index in range(0, total_pages):
-        for v in session.query(Vote).filter(Vote.id > last_id).limit(page).all():
-            last_id = v.id
-            if type(v.started) == bool:
-                if now - v.creation_date > timedelta(hours=3):
-                    delete_all(vote_id=v.id)
-                    deleted_votes += 1
-            else:
-                if now - v.finished_date > timedelta(hours=1):
-                    delete_all(vote_id=v.id)
-                    deleted_votes += 1
+        last_id = 0
+        for index in range(0, total_pages):
+            for v in session.query(Vote).filter(Vote.id > last_id).limit(per_page).all():
+                last_id = v.id
+                if type(v.started) == bool:
+                    if now - v.creation_date > timedelta(hours=3):
+                        delete_all(vote_id=v.id)
+                        deleted_votes += 1
+                else:
+                    if now - v.finished_date > timedelta(hours=1):
+                        delete_all(vote_id=v.id)
+                        deleted_votes += 1
 
-    e(f"deleted_votes = {deleted_votes}")
-    e(f"elapsed time  = {round(time() - a, 2)}s")
-    wait()
-
-
-def wait():
-    try:
-        sleep(timedelta(minutes=30).seconds)
-        main()
-    except KeyboardInterrupt:
-        return 0
+        logger.info(f"{deleted_votes} votes deleted")
 
 
 if __name__ == "__main__":
-    init_engine()
-    init_logger()
+    load_dotenv()
+    logger.setLevel(INFO)
+    handler = StreamHandler()
+    handler.setFormatter(fmt=Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S"))
+    logger.addHandler(hdlr=handler)
 
-    e("===== worker  started =====")
+    engine = create_engine(environ.get("SQLALCHEMY_DATABASE_URI"))
+    session_factory = sessionmaker(bind=engine)
+
     main()
-    e("===== worker finished =====")
+
+    try:
+        s.run()
+    except KeyboardInterrupt:
+        logger.info("exited!")
