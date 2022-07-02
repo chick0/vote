92c584468a1f9377fe688d0c89bdd8b93df4bcaf
chick_0 <chick_0@ch1ck.xyz>
2022-07-02 17:07:52 +0900

    add except handler

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,15 @@ from fastapi import FastAPI
 
 from app.init import init
 
+try:
+    version = run("git rev-parse --short HEAD", capture_output=True).stdout.strip().decode()
+except:
+    version = "fail to fetch version"
+
 app = FastAPI(
     title="Vote API & WS",
     description="QR코드로 참여하는 단순 투표 서비스",
-    version=run("git rev-parse --short HEAD", capture_output=True).stdout.strip().decode(),
+    version=version,
     openapi_url="/openapi.json" if '--dev' in argv else None
 )
 
