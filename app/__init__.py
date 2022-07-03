2ecd7af930f7e2ea2947e456b91b8d42ebfc827c
chick_0 <chick_0@ch1ck.xyz>
2022-07-03 21:38:50 +0900

    get head from `.git`

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from sys import argv
+from os.path import join
 from subprocess import run
 
 from fastapi import FastAPI
@@ -7,8 +8,15 @@ from app.init import init
 
 try:
     version = run("git rev-parse --short HEAD", capture_output=True).stdout.strip().decode()
+    raise NotImplementedError
 except:
-    version = "fail to fetch version"
+    version = None
+
+if version is None:
+    try:
+        version = open(join(".git", open(join(".git", "HEAD"), mode="r").read()[5:].strip()), mode="r").read()[:7]
+    except:
+        version = "fail to get version"
 
 app = FastAPI(
     title="Vote API & WS",
