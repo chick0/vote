7e66a17e710d56361ea82e0007122979f5e6ae0e
chick_0 <chick_0@ch1ck.xyz>
2022-07-15 19:48:15 +0900

    bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,14 @@
 from sys import argv
 from os.path import join
-from subprocess import run
 
 from fastapi import FastAPI
 
 from app.init import init
 
 try:
-    version = run("git rev-parse --short HEAD", capture_output=True).stdout.strip().decode()
-    raise NotImplementedError
-except:
-    version = None
-
-if version is None:
-    try:
-        version = open(join(".git", open(join(".git", "HEAD"), mode="r").read()[5:].strip()), mode="r").read()[:7]
-    except:
-        version = "fail to get version"
+    version = open(join(".git", open(join(".git", "HEAD"), mode="r").read()[5:].strip()), mode="r").read()[:7]
+except (FileNotFoundError, Exception):
+    version = "*MISSING HEAD*"
 
 app = FastAPI(
     title="Vote API & WS",
